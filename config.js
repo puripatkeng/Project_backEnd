2aab6dcf4af5903bc53bbb82b109fa24f3712a79
Puripat Laiprasert <puripat_keng@hotmail.com>
2022-08-24 18:36:45 +0700

    fix again

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 
-module.exports = {
-  port: process.env.PORT || 4001,
+const config = {
+  port: +process.env.PORT || 4001,
   mongoUri: process.env.MONGO_URI,
   mongoOptions: {
     user: process.env.MONGO_USER,
     pass: process.env.MONGO_PASSWORD,
     dbName: process.env.MONGO_DATABASE,
     retryWrites: true,
-    w: 'majority',
+    w: "majority",
   },
 };
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require("express");
 const mongoose = require("mongoose");
 const app = require("./api/index.js");
 const config = require("./config");
 
 const boot = async () => {
   await mongoose
-    .connect(await mongoose.connect(config.mongoUri, config.mongoOptions))
+    .connect(config.mongoUri, config.mongoOptions)
     .then(() => app.listen(8080))
     .then(() =>
       console.log("Connected TO Database and Listening TO Localhost 8080")
