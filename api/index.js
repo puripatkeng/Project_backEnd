b1e48934e90061e32b58d0de51fcf61932b480c3
Puripat Laiprasert <puripat_keng@hotmail.com>
2022-08-24 16:31:25 +0700

    fix fix

--- api/index.js
+++ api/index.js
@@ -1,21 +1,18 @@
-require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const controllerMongoose = require("./src/controller/mongoose-controller");
-const cookieParser = require("cookie-parser");
-// const session = require("express-session");
+
 const corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200,
   credentials: true,
 };
-const routerIndex = require("./routerIndex");
+const routerIndex = require("../routerIndex");
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(cors(corsOptions));
-app.use(cookieParser());
 
-app.use(controllerMongoose.connectMongoose);
 app.use(routerIndex);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -2,9 +2,29 @@
 //   res.send(req.cookies);
 // });
 // const authSession = require("./src/middlewares/authSession");
-require("dotenv").config();
-const express = require("express");
-const app = express();
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
-});
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is listening on port ${process.env.PORT}`);
+// });
+
+const mongoose = require("mongoose");
+const app = require("./api/index");
+
+const config = require("./config");
+
+const boot = async () => {
+  // Connect to mongodb
+  await mongoose.connect(config.mongodb.uri, {
+    user: process.env.MONGO_USER,
+    pass: process.env.MONGO_PASSWORD,
+    retryWrites: true,
+  });
+  // Start express server
+  app.listen(config.port, () => {
+    console.log(`Server is listening on port ${config.port}`);
+  });
+};
+
+boot();
